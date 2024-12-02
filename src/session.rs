c325a4039311a164c1cb636f9ffdfa6e46c0ba99
Taylor Vann <brian.t.vann@gmail.com>
2024-12-02 13:35:39 -0800

    sessions

---
+++ src/session.rs
@@ -0,0 +1,63 @@
+const session_url_path: &str = "/session";
+const session_error_code: usize = 500;
+const session_error: &str = "session not created"
+
+// // POST /session
+// struct PostSessionResponse {
+// 	session_id: String;
+// }
+
+// // Create session
+// // Delete session
+
+// add capabilities later
+
+const session_url_path: &str = "/session"
+
+struct Error {
+	error: String;
+	message: String;
+	stacktrace: String;
+}
+
+struct ProxyConfiguration {
+	proxy_type: String;
+	proxy_auto_config_url: String;
+	ftp_proxy: String;
+	http_proxy: String;
+	no_proxy: Vec<String>;
+	ssl_proxy: String;
+	socks_proxy: String;
+	socks_version: usize;
+}
+
+struct SessionCapabilities {
+	browser_name: String;
+	browser_version: String;
+	platform_name: String;
+	accepts_insecure_certs: bool;
+	page_load_strategy: String;
+	proxy: ProxyConfiguration;
+	set_window_rect: bool;
+	strict_file_interactability: bool;
+	unhandled_prompt_behavior: String;
+	user_agent: String;
+}
+
+struct NewSessionJSONResposne {
+	session_id: String,
+	capabilities: SessionCapabilities;
+}
+
+pub fn new_session_request() {
+	// add POST
+	//
+	// 
+}
+
+pub fn delete_session_request(session_id: String) {
+	// add DELETE
+	// /session/<session_id>
+	//
+	// success if return value is null
+}
